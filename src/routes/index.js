167a9b8966d033f8256ed12c3940206b48b5a18b
morpheusec <morpheusec@gmail.com>
2020-07-08 06:31:07 -0500

    mi primer commit

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+// routes
+router.get('/',(req,res) => {
+    res.render('index.html',{title: 'First Website'});
+
+});
+
+router.get('/contact',(req,res) => {
+    res.render('contact.html',{title: 'Contact page'});
+
+});
+
+
+module.exports = router;
+
